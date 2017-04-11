c9bf1529a8aa9c38a8ab0dfbc5050db5f55f8668
jingliang <jingliang@fotoplace.cc>
2017-04-11 15:17:04 +0800

    e

---
+++ ImageToColor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageToColor
+//
+//  Created by jingliang on 2017/4/11.
+//  Copyright © 2017年 井良. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageToColor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageToColor
+//
+//  Created by jingliang on 2017/4/11.
+//  Copyright © 2017年 井良. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
